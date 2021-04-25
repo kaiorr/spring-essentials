85eeecacc63822874af01589f6b06ab4fd9fea57
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-04-25 19:47:56 -0300

    GET total e GET por id, também foi customizado mesagem de retorno ao acessar uma rota incorreta

--- src/main/java/com/kaio/ribeiro/springessentials/controller/AnimeController.java
+++ src/main/java/com/kaio/ribeiro/springessentials/controller/AnimeController.java
@@ -5,6 +5,10 @@ import com.kaio.ribeiro.springessentials.domain.Anime;
 import com.kaio.ribeiro.springessentials.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +23,15 @@ public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
-    public List<Anime> list() {
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<List<Anime>> findById(@PathVariable long id) {
+        return new ResponseEntity(animeService.findById(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/kaio/ribeiro/springessentials/service/AnimeService.java
+++ src/main/java/com/kaio/ribeiro/springessentials/service/AnimeService.java
@@ -2,13 +2,25 @@ package com.kaio.ribeiro.springessentials.service;
 
 import com.kaio.ribeiro.springessentials.domain.Anime;
 import com.kaio.ribeiro.springessentials.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime( 1L, "Boku No Hero"), new Anime( 2L, "Berserk"));
+
     public List<Anime> listAll() {
-     return List.of(new Anime( 1L, "Boku No Hero"), new Anime( 2L, "Berserk"));
+     return animes;
     }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found :( "));
+
+    }
+
 }
