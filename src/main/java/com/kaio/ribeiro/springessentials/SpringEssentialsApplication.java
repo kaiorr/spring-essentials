bfd99091267eeb127d0aa73aac234cbec4100c23
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-04-25 16:04:30 -0300

    initial commit

---
+++ src/main/java/com/kaio/ribeiro/springessentials/SpringEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.kaio.ribeiro.springessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEssentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaio/ribeiro/springessentials/SpringEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaio.ribeiro.springessentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
