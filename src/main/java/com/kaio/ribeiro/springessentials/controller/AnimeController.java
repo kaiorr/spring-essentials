d2d7ce4dfada2ee5c346c5e8d9913097e9fcc608
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-04-25 17:45:44 -0300

    Iniciado mvc de animes para estudo de comportamento

---
+++ src/main/java/com/kaio/ribeiro/springessentials/controller/AnimeController.java
@@ -0,0 +1,26 @@
+package com.kaio.ribeiro.springessentials.controller;
+
+import com.kaio.ribeiro.springessentials.Util.DateUtil;
+import com.kaio.ribeiro.springessentials.domain.Anime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+    }
+}
---
+++ src/main/java/com/kaio/ribeiro/springessentials/domain/Anime.java
@@ -0,0 +1,17 @@
+package com.kaio.ribeiro.springessentials.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
